a8be9fc8ec9f2246566344a442bd39f77756cf4d
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-09-07 23:34:06 +0300

    Добавлены кнопки для вк

---
+++ TravelerBot.Api/Services/Logic/MenuKeyboard.cs
@@ -0,0 +1,164 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelerBot.Api.ResourceModels;
+using TravelerBot.Api.Services.Interfaces;
+
+namespace TravelerBot.Api.Services.Logic
+{
+    public class MenuKeyboard : IKeyboard
+    {
+        public ResponseModel Get(string buttonName)
+        {
+            var message = "Укажите все необходимые пункты";
+
+            var buttons = new List<Button>
+                {
+                    new Button
+                    {
+                        Color = (buttonName == "Водитель") ? "positive" : "default",
+                        Action = new Action
+                        {
+                            Label = "Водитель",
+                            Type = "text",
+                            Payload = JsonConvert.SerializeObject(new
+                            {
+                                button = "1"
+                            })
+                        }
+                    },
+                    new Button
+                    {
+                        Color = (buttonName == "Пассажир") ? "positive" : "default",
+                        Action = new Action
+                        {
+                            Label = "Пассажир",
+                            Type = "text",
+                            Payload = JsonConvert.SerializeObject(new
+                            {
+                                button = "2"
+                            })
+                        }
+                    }
+                }.ToArray();
+
+            var buttonsFromTo = new List<Button>
+                {
+                    new Button
+                    {
+                        Color = "default",
+                        Action = new Action
+                        {
+                            Label = "Откуда",
+                            Type = "text",
+                            Payload = JsonConvert.SerializeObject(new
+                            {
+                                button = "3"
+                            })
+                        }
+                    },
+                    new Button
+                    {
+                        Color = "default",
+                        Action = new Action
+                        {
+                            Label = "Куда",
+                            Type = "text",
+                            Payload = JsonConvert.SerializeObject(new
+                            {
+                                button = "4"
+                            })
+                        }
+                    }
+                }.ToArray();
+
+            var buttonsDateTime = new List<Button>
+                {
+                    new Button
+                    {
+                        Color = "default",
+                        Action = new Action
+                        {
+                            Label = "Когда",
+                            Type = "text",
+                            Payload = JsonConvert.SerializeObject(new
+                            {
+                                button = "5"
+                            })
+                        }
+                    },
+                    new Button
+                    {
+                        Color = "default",
+                        Action = new Action
+                        {
+                            Label = "Во сколько",
+                            Type = "text",
+                            Payload = JsonConvert.SerializeObject(new
+                            {
+                                button = "6"
+                            })
+                        }
+                    }
+                }.ToArray();
+
+            var buttonsDescription = new List<Button>
+                {
+                    new Button
+                    {
+                        Color = "default",
+                        Action = new Action
+                        {
+                            Label = "Комментарии",
+                            Type = "text",
+                            Payload = JsonConvert.SerializeObject(new
+                            {
+                                button = "7"
+                            })
+                        }
+                    }
+                }.ToArray();
+
+            var startKeyboard = new List<Button>
+                    {
+                        new Button
+                        {
+                            Color = "default",
+                            Action = new Action
+                            {
+                                Label = "Перейти на начало",
+                                Type = "text",
+                                Payload = JsonConvert.SerializeObject(new
+                                {
+                                    button = "8"
+                                })
+                            }
+                        }
+                    }.ToArray();
+
+            var keyboard = new Keyboard
+            {
+                OneTime = false,
+                Buttons = new[]
+                {
+                    buttons,
+                    buttonsFromTo,
+                    buttonsDateTime,
+                    startKeyboard
+                }
+            };
+
+            return new ResponseModel
+            {
+                Message = message,
+                Keyboard = keyboard
+            };
+        }
+
+        public ResponseModel Get()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ TravelerBot.Api/Services/Logic/StartKeyboard.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using TravelerBot.Api.ResourceModels;
+using TravelerBot.Api.Services.Interfaces;
+
+namespace TravelerBot.Api.Services.Logic
+{
+    public class MainKeyboard : IKeyboard
+    {
+        public ResponseModel Get()
+        {
+            var message = "Найти поездку или добавить новую поездку?";
+
+            var buttons = new List<Button>
+                {
+                    new Button
+                    {
+                        Color = "default",
+                        Action = new Action
+                        {
+                            Label = "Найти поездку",
+                            Type = "text",
+                            Payload = JsonConvert.SerializeObject(new
+                            {
+                                button = "1"
+                            })
+                        }
+                    },
+                    new Button
+                    {
+                        Color = "default",
+                        Action = new Action
+                        {
+                            Label = "Добавить поездку",
+                            Type = "text",
+                            Payload = JsonConvert.SerializeObject(new
+                            {
+                                button = "2"
+                            })
+                        }
+                    }
+                }.ToArray();
+
+            var startKeyboard = new List<Button>
+                    {
+                        new Button
+                        {
+                            Color = "default",
+                            Action = new Action
+                            {
+                                Label = "Перейти на начало",
+                                Type = "text",
+                                Payload = JsonConvert.SerializeObject(new
+                                {
+                                    button = "3"
+                                })
+                            }
+                        }
+                    }.ToArray();
+
+            var keyboard = new Keyboard
+            {
+                OneTime = false,
+                Buttons = new[] { buttons, startKeyboard }
+            };
+
+            return new ResponseModel
+            {
+                Message = message,
+                Keyboard = keyboard
+            };
+        }
+    }
+}
