c4fdd5d11c1c8298af2fbc1d04e965eb36ab325c
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-09-12 12:29:49 +0500

    Добавлены классы для работы с кнопками

---
+++ TravelerBot.MVC/Data/Repositories/Interfaces/ITripRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using TravelerBot.MVC.Models;
+
+namespace TravelerBot.Api.Data.Repositories
+{
+    public interface ITripRepository
+    {
+        void Add(Trip trip);
+
+        void Update(Trip trip);
+
+        Trip Get(int accountVkontakteId);
+    }
+}
---
+++ TravelerBot.MVC/Data/Repositories/Logic/TripRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using TravelerBot.Api.Data.Repositories;
+using TravelerBot.MVC.Models;
+
+namespace TravelerBot.MVC.Data.Repositories
+{
+    public class TripRepository : ITripRepository
+    {
+        TripContext tripContext = new TripContext();
+
+        public Trip Get(int accountId)
+        {
+            return tripContext.Trips.FirstOrDefault(t => t.AccountId == accountId);
+        }
+
+        public void Add(Trip trip)
+        {
+            tripContext.Trips.Add(trip);
+            tripContext.SaveChanges();
+        }
+
+        public void Update(Trip trip)
+        {
+            tripContext.Entry(trip).State = System.Data.Entity.EntityState.Modified;
+            tripContext.SaveChanges();
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    tripContext.Dispose();
+        //    base.Dispose(disposing);
+        //}
+    }
+}
